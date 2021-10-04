665466beb0df8e726ada81d5c870a8315ecd4765
Samarth <Samarth@Samarth-PC>
2021-10-04 11:55:01 +0530

    up

--- src/gittest/Test.java
+++ src/gittest/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is the sample git project");
 		System.out.println("This is the sample git project");
+		System.out.println("This is up the sample git project");
 	}
 
 }
